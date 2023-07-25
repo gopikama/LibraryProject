77325668750ec09698af1851051d6bbd407870a9
Gopika Mahadevan <mgopika3000@gmail.com>
2023-07-25 17:30:16 -0500

    switch case issue fixed

--- src/LibraryProject/Library.java
+++ src/LibraryProject/Library.java
@@ -35,28 +35,35 @@ public class Library {
             {case 1:{   System.out.println("Please enter name of item to be returned");
                 itemName=br.readLine();
                 ItemReturn returnObject=new ItemReturn(itemName);
+                break;
             }
             case 2:{ System.out.println("Please enter name of item to be renewed");
                     itemName=br.readLine();
                     ItemRenew renewObject=new ItemRenew(itemName);
+                    break;
 
             }
                 case 3:{
                     System.out.println("Please enter name of item to be requested");
                     itemName=br.readLine();
                     ItemRequest requestObject=new ItemRequest(itemName);
+                    break;
 
                 }
                 case 4:{
                     User userObject=new User(libraryCardNumber);
+                    userObject.displayUserDetails();
+                    break;
 
                 }
                 case 5:{
                     break;
 
                 }
-
             }
+            System.out.println("Do you want to continue ,if you want to exit press 5 otherwise enter a choice(1/2/3/4):");
+            choice=Integer.parseInt(br.readLine());
+
 
 
         }while(choice!=5);
--- src/LibraryProject/User.java
+++ src/LibraryProject/User.java
@@ -1,10 +1,25 @@
 package LibraryProject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class User {
+    String name;
+    String address;
+    Long phoneNumber;
     int libraryCardNumber;
+    int age;
+    Float totalOverDueFine;
+
+
     public User(int libraryCardNumber){
+
         this.libraryCardNumber=libraryCardNumber;
     }
+    public void displayUserDetails() {
+        System.out.println("The details for all users are:");
 
-
+    }
 }
